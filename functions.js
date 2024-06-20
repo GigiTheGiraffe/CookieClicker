d689db0fbce53107ace81da88ccfb480a004fa2d
Martin Loic <loic.martin2000@gmail.com>
2024-06-20 16:34:43 +0200

    ajout d'une fonction pour changer l'état d'un bouton

--- functions.js
+++ functions.js
@@ -2,24 +2,34 @@ let score = 0;
 let clickValue = 1;
 const cookieBtn = document.querySelector(".cookieBtn");
 const scoreDisplay = document.querySelector(".scoreDisplay");
-console.log(cookieBtn);
 
 function click() {
     score += clickValue;
-}
+};
 
 function updateScoreDisplay() {
     scoreDisplay.textContent = `Total : ${score}`;
-}
+};
 
 cookieBtn.addEventListener("click", () => {
     //ajout de clickValue à score
     click();
     //changer la valeur du score
     updateScoreDisplay();
-})
+});
 
-// fonction pour enlever la class active d'un bouton et y mettre la classe passive
-function disableButton(buttonName) {
-    buttonName.classList.remove(a)
+//fonction qui change la source d'une image en lui donnant une nouvelle source
+function changeSrcImg(image, source) {
+    image.src = source
 }
+
+// fonction pour changer la class active/disabled d'un bouton en fonction de si activate vaut true ou false et de changer l'image. 
+function toggleButton(button, classBtn, image, source, activate) {
+    if (activate) {
+        button.classList.add(classBtn);
+    } else {
+        button.classList.remove(classBtn);
+    }
+    changeSrcImg(image, source);
+}
+
